5dae70ed359d7e1509c8ed9e17ead3a7c170aaf5
meltrust <original.mtapia@outlook.com>
2021-03-19 16:29:40 -0600

    generate validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates :url, presence: true, length: { minimum: 25 }
+  validates :body, presence: true, length: { minimum: 50 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { minimum: 3 }
+  validates :email, presence: true, length: { minimum: 10 }
 end
