0b876055665b0c9b32b9fc3b84a813261138e1aa
meltrust <original.mtapia@outlook.com>
2021-03-19 15:52:49 -0600

    generate more models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_214350) do
+ActiveRecord::Schema.define(version: 2021_03_19_215142) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment_body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "url"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
